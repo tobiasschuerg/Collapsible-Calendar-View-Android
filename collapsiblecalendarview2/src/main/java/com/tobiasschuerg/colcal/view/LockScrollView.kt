d38ccdf4e136e317536caba8695dded1e060af57
Tobias Schürg <tobiasschuerg@gmail.com>
2018-11-10 18:12:25 +0100

    Updated gradle wrapper.

--- collapsiblecalendarview2/src/main/java/com/tobiasschuerg/colcal/view/LockScrollView.kt
+++ collapsiblecalendarview2/src/main/java/com/tobiasschuerg/colcal/view/LockScrollView.kt
@@ -1,16 +1,23 @@
 package com.tobiasschuerg.colcal.view
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.widget.ScrollView
 
-class LockScrollView(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ScrollView(context, attrs, defStyleAttr) {
+
+class LockScrollView @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : ScrollView(context, attrs, defStyleAttr) {
 
     override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
         return false
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(ev: MotionEvent): Boolean {
         return false
     }
